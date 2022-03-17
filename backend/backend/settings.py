4a87a650b63d1da2bf0d1aaa7765bc1b8c94968b
myusername <igor.mazur@hotmail.com>
2022-03-17 23:39:08 +0100

    login working

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -45,6 +45,8 @@ INSTALLED_APPS = [
     'corsheaders'
 ]
 
+CORS_ORIGIN_ALLOW_ALL = True
+
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
