924dba56b8b85ac605447c606f8c3774b2791f89
myusername <igor.mazur@hotmail.com>
2022-04-01 09:32:49 +0200

    registration page, dark mode, UI design, validation of registration form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,67 @@
-import './App.css';
-import Main from './components/Main.js'
+import * as React from 'react';
+import IconButton from '@mui/material/IconButton';
+import { useTheme, ThemeProvider, createTheme } from '@mui/material/styles';
+import Brightness4Icon from '@mui/icons-material/Brightness4';
+import Brightness7Icon from '@mui/icons-material/Brightness7';
+import Register from './components/Register';
 import Login from './components/Login';
-import MainUser from './components/mainuser/MainUser.js';
-import SecondUser from './components/seconduser/SecondUser.js';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'jquery/dist/jquery.min.js';
-import 'bootstrap/dist/js/bootstrap.js'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import MainUser from './components/mainuser/MainUser';
+import SecondUser from './components/seconduser/SecondUser';
+import { Routes, Route, BrowserRouter as Router, Navigate } from 'react-router-dom';
+import Cookies from "js-cookie";
 
-export const App = (props) => {
 
+
+const ColorModeContext = React.createContext({ toggleColorMode: () => { } });
+
+Cookies.get("mode",) === 'dark' ? Cookies.set("mode", 'dark') :  Cookies.set("mode", 'light')
+
+function ThemeButton() {
+  const theme = useTheme();
+  const colorMode = React.useContext(ColorModeContext);
+  Cookies.set("mode", theme.palette.mode)
   return (
-    <div>
-      <Router>
-        <Routes>
-          <Route path='/' element={<Main />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/mainuser' element={<MainUser />} />
-          <Route path='/seconduser' element={<SecondUser />} />
-        </Routes>
-      </Router>
-    </div>
-  )
+    <IconButton sx={{ ml: 0 }} onClick={colorMode.toggleColorMode} color="inherit">
+      {Cookies.get("mode") === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
+    </IconButton>
+  );
 }
 
-export default App;
+export default function ToggleColorMode() {
+  const [mode, setMode] = React.useState(Cookies.get("mode"));
+  const colorMode = React.useMemo(
+    () => ({
+      toggleColorMode: () => {
+        setMode((prevMode) => (prevMode === 'dark' ? 'light' : 'dark'));
+      },
+    }),
+    [],
+  );
+
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode,
+        },
+      }),
+    [mode],
+  );
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <Router > {/* The Switch decides which component to show based on the current URL.*/}
+          <Routes>
+            <Route path="/" element={<Navigate replace to='/login' />} />
+            <Route path='/register' element={<Register button={<ThemeButton />}/>}/>
+            <Route path="/login" element={<Login button={<ThemeButton />}/>} />
+            <Route path="/mainuser" element={<MainUser button={<ThemeButton />}/>} />
+            <Route path="/seconduser" element={<SecondUser button={<ThemeButton />}/>} />
+          </Routes>
+        </Router >
+
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
+}
---
+++ frontend/src/components/Login.jsx
@@ -0,0 +1,213 @@
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { InputAdornment, IconButton } from '@mui/material';
+import AlternateEmailIcon from '@mui/icons-material/AlternateEmail';
+import LoginIcon from '@mui/icons-material/Login'
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import './Login.css';
+import authService from '../services/auth.service';
+import { useLocation } from 'react-router';
+import { useNavigate } from 'react-router-dom';
+
+function Copyright(props) {
+    return (
+        <Typography variant="body2" color="text.secondary" align="center" {...props}>
+            {'Copyright © '}
+            <Link color="inherit" href="https://dnastrands.pl/">
+                dnastrands.pl
+            </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    );
+}
+
+const theme = createTheme();
+
+export default function SignIn(props) {
+
+    const navigate = useNavigate();
+    const { state } = useLocation();
+    // const { data } = state;
+    const startValid = { isValid: "no", errorText: "", focused: false }
+    const [passwordVisibility, setPasswordVisibility] = React.useState(false);
+    const [passwordValidation, setPasswordValidation] = React.useState(startValid);
+    const [emailValidation, setEmailValidation] = React.useState(startValid);
+    const [passwordInput, setPasswordInput] = React.useState("password");
+
+    const handleVisibilityClick = () => {
+        setPasswordVisibility(!passwordVisibility);
+        setPasswordInput(passwordVisibility ? "password" : "text");
+    }
+
+    const emptyValidation = (event, setField, prev) => {
+        if (event)
+            setField(prev => ({
+                ...prev,
+                isValid: true,
+                errorText: "",
+                focused: true,
+            }));
+        else
+            setField(prev => ({
+                ...prev,
+                isValid: false,
+                errorText: "Pole nie może byc puste",
+                focused: false
+            }));
+    }
+
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        const username = data.get('email');
+        const password = data.get('password');
+
+        authService.login(username, password, data).then(
+            (success) => {
+                if (data === 'user') {
+                    navigate('/seconduser');
+                } else {
+                    navigate('/mainuser');
+                }
+            },
+            (error) => {
+                alert('You entered wrong username or password!');
+            }
+        );
+        console.log({
+            username: data.get('email'),
+            password: data.get('password'),
+        });
+    };
+
+    // const sumbitLogin = (event) => {
+    //   event.preventDefault();
+    //   const username = event.target[0].value;
+    //   const password = event.target[1].value;
+    //   // const data = new FormData(event.currentTarget);
+    //   authService.login(username, password, data).then(
+    //     (success) => {
+    //       if (data === 'user') {
+    //         navigate('/dashboard');
+    //       } else {
+    //         navigate('/restaurantview');
+    //       }
+    //     },
+    //     (error) => {
+    //       alert('You entered wrong username or password!');
+    //     }
+    //   );
+    // };
+
+    return (
+        <Container component="main" maxWidth="xs" className="center">
+            <CssBaseline />
+            <Box
+                sx={{
+                    marginTop: 8,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                }}
+            >
+                <img src={require("../images/dnalogo.png")} width="150" height="75" alt="zamów jedzenie" />
+
+                <Typography component="h1" variant="h5">
+                    {props.button}
+                    Logowanie
+                </Typography>
+                <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+                    <TextField
+                        onChange={(event) => { emptyValidation(event.target.value, setEmailValidation, emailValidation) }}
+                        InputProps={{
+                            endAdornment:
+                                <InputAdornment position="end">
+                                    <LoginIcon />
+                                </InputAdornment>
+                            ,
+                        }}
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="email"
+                        label="Login"
+                        name="email"
+                        autoComplete="email"
+                        autoFocus
+                        focused={emailValidation.focused}
+                        error={!emailValidation.isValid}
+                        helperText={emailValidation.errorText}
+                        color="primary"
+                    />
+                    <TextField
+                        onChange={(event) => { emptyValidation(event.target.value, setPasswordValidation, passwordValidation) }}
+                        InputProps={{
+                            endAdornment:
+                                <InputAdornment position="end">
+                                    <IconButton
+                                        aria-label="toggle password visibility"
+                                        edge="end"
+                                        onClick={handleVisibilityClick}
+                                    >
+                                        {passwordVisibility ? <Visibility /> : <VisibilityOff />}
+                                    </IconButton>
+                                </InputAdornment>
+                        }}
+                        margin="normal"
+                        required
+                        fullWidth
+                        name="password"
+                        label="Hasło"
+                        type={passwordInput}
+                        id="password"
+                        autoComplete="current-password"
+                        focused={passwordValidation.focused}
+                        error={!passwordValidation.isValid}
+                        helperText={passwordValidation.errorText}
+                        color="primary"
+                    />
+                    <FormControlLabel
+                        control={<Checkbox value="remember" color="primary" />}
+                        label="Zapamiętaj mnie"
+                    />
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        sx={{ mt: 3, mb: 2 }}
+                    >
+                        Zaloguj się
+                    </Button>
+                    <Grid container>
+                        <Grid item xs>
+                            <Link href="/forgotten-password" variant="body2">
+                                Zapomniałeś hasła?
+                            </Link>
+                        </Grid>
+                        <Grid item>
+                            <Link href="/register" variant="body2">
+                                {"Załóż konto"}
+                            </Link>
+                        </Grid>
+                    </Grid>
+                </Box>
+            </Box>
+            <Copyright sx={{ mt: 8, mb: 4 }} />
+        </Container>
+    );
+}
